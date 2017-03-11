bc8342bdf385618614c968c76b7deed6ed088f28
bowwow <bowwow.bread@gmail.com>
2017-03-11 19:45:00 +0900

    mypage

---
+++ src/components/mypage/mypage.vue
@@ -0,0 +1,56 @@
+<template>
+    <div id='mypage'>
+        <div class='myinfo'>
+            <ul>
+                <li>
+                    <p>이름</p>
+                    <p>학번</p>
+                </li>
+            </ul>
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'index',
+    data() {
+        return {
+            userToken: '',
+        };
+    },
+    beforeCreate() {
+	    const ROOT_URL = 'http://121.186.23.245:9999';
+	    this.$http.defaults.baseURL = ROOT_URL;
+
+//          토큰 테스트
+	    this.userToken = this.$cookie.get('userToken');
+	    if (this.userToken != null) {
+		    this.userToken = this.$cookie.get('userToken');
+		    this.$http.defaults.headers.common.Authorization = this.userToken;
+		    this.$http.get('/api/users/my-info')
+		    .then((resInfo) => {
+			    if (resInfo.status === 200) {
+				    this.userid = resInfo.data.user.userId;
+			    }
+		    })
+		    .catch((error) => {
+			    alert(error);
+		    });
+	    } else {
+            this.$swal({
+                title: '입장 실패',
+                text: '로그인을 해주세요',
+                type: 'error',
+            })
+            .then(() => {
+                location.href = '/';
+            });
+        }
+    },
+};
+</script>
+<style scoped>
+
+</style>
