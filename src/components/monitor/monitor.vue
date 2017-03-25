c52579920b8810478ec5e32a9c3bbf945dcb0782
awseq2414 <awseq2414@naver.com>
2017-03-26 01:46:57 +0900

    monitor design

--- src/components/monitor/monitor.vue
+++ src/components/monitor/monitor.vue
@@ -2,37 +2,80 @@
 <div id="monitor">
     <div class="button">
         <div class="wrapper">
-            <h1>Menu</h1>
-            <a class="menu-btn" onclick="toggleMenu()"></a>
-            <section class="one" onclick="goToPage(0)">
-                <h1>Profile</h1>
-            </section>
-            <section class="two" onclick="goToPage(1)">
-                <h1>Friends</h1>
-            </section>
-            <section class="three" onclick="goToPage(2)">
-                <h1>Messages</h1>
-            </section>
-            <section class="four" onclick="goToPage(3)">
-                <h1>Settings</h1>
+            <div class="four">
+              <div class="btn">
+                <button class="ui button">
+                  On
+                </button>
+                <button class="ui button">
+                  Off
+                </button>
+              </div>
                 <div class="ui cards">
-                    <div class="card" id="card">
+                    <div class="card" id="mncard">
                         <div class="content">
-                            <div class="header"><p>아이디 : <span>admin</span></p></div>
-                            <div class="meta"><p>학번 : <span>10626</span></p></div>
-                            <div class="meta"><p>이름 : <span>기무띠</span></p></div>
+                            <div class="header"><a href="#">일반문제</a></div>
+                            <div class="header"><a href="#">대회문제</a></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="ui cards">
+                    <div class="card" id="topcard">
+                        <div class="content">
+                            <div class="contentin">
+                                <div class="header">
+                                    <p>푼 문제</p>
+                                </div>
+                                <div class="header">
+                                    <p>맞춘 문제</p>
+                                </div>
+                                <div class="header">
+                                    <p>틀린 문제</p>
+                                </div>
+                            </div>
                             <hr>
-                            <div class="description">
-                                <p>코드 : <span>12313124</span></p>
-                                <p>제출 결과 : <span>true / false</span></p>
+                            <div class="content">
+                                <div class="contentin">
+                                    <p>1,232</p>
+                                    <p>1,232</p>
+                                    <p>1,232</p>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div class="card" id="card">
+                </div>
+                <div class="ui cards">
+                    <div class="card">
                         <div class="content">
-                            <div class="header"><p>아이디 : <span>admin</span></p></div>
-                            <div class="meta"><p>학번 : <span>10626</span></p></div>
-                            <div class="meta"><p>이름 : <span>기무띠</span></p></div>
+                            <div class="header">
+                                <p>아이디 : <span>admin</span></p>
+                            </div>
+                            <div class="meta">
+                                <p>학번 : <span>10626</span></p>
+                            </div>
+                            <div class="meta">
+                                <p>이름 : <span>기무띠</span></p>
+                            </div>
+                            <div class="meta">
+                                <p>제출 결과 : <span>true / false</span></p>
+                            </div>
+                            <hr>
+                            <div class="description">
+                                <p>코드 : <span>12313124123131241231312412313124</span></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="content">
+                            <div class="header">
+                                <p>아이디 : <span>admin</span></p>
+                            </div>
+                            <div class="meta">
+                                <p>학번 : <span>10626</span></p>
+                            </div>
+                            <div class="meta">
+                                <p>이름 : <span>기무띠</span></p>
+                            </div>
                             <hr>
                             <div class="description">
                                 <p>코드 : <span>12313124</span></p>
@@ -41,51 +84,53 @@
                         </div>
                     </div>
                 </div>
-            </section>
+            </div>
+        </div>
     </div>
-    </div>
-    </div>
+</div>
 </template>
 
 <script>
-    export default {
-        name: 'monitor',
-        data() {
-            return {
+export default {
+    name: 'monitor',
+    data() {
+        return {
 
-            };
+        };
+    },
+    created() {},
+    mounted() {
+        this.$nextTick(() => {
+            this.$store.commit('loadingOff');
+            this.$Progress.finish();
+        });
+    },
+    methods: {
+        monitoring() {
+            this.$http.get('solution')
+                .then((res) => {
+                    let i = 0;
+                    while (i < res.data.resolves.length) {
+                        const data = res.data.resolves[i].resolveData.data.replace('T', ', ');
+                        this.monitorData.push({
+                            userid: res.data.resolves[i].userId,
+                            code: res.data.resolves[i].resolveData.code,
+                            date: data.substring(0, data.length - 8),
+                            lang: res.data.resolves[i].resolveData.language,
+                            memory: res.data.resolves[i].resolveData.memory,
+                            num: res.data.resolves[i].resolveData.problemNum,
+                            result: res.data.resolves[i].resolveData.result,
+                            time: res.data.resolves[i].resolveData.time,
+                        });
+                        i += 1;
+                    }
+                });
         },
-        created() {},
-        mounted() {
-            this.$nextTick(() => {
-                this.$store.commit('loadingOff');
-                this.$Progress.finish();
-            });
-        },
-        methods: {
-            monitoring() {
-                this.$http.get('solution')
-                    .then((res) => {
-                        let i = 0;
-                        while (i < res.data.resolves.length) {
-                            const data = res.data.resolves[i].resolveData.data.replace('T', ', ');
-                            this.monitorData.push({
-                                userid: res.data.resolves[i].userId,
-                                code: res.data.resolves[i].resolveData.code,
-                                date: data.substring(0, data.length - 8),
-                                lang: res.data.resolves[i].resolveData.language,
-                                memory: res.data.resolves[i].resolveData.memory,
-                                num: res.data.resolves[i].resolveData.problemNum,
-                                result: res.data.resolves[i].resolveData.result,
-                                time: res.data.resolves[i].resolveData.time,
-                            });
-                            i += 1;
-                        }
-                    });
-            },
-        },
-    };
+    },
+};
 </script>
+
+
 
 
 
